ec7b3dd0de0f7c589edfd6c66b4f6e504d698a73
Keith Irwin <git@keithirwin.us>
2019-06-26 10:53:34 -0500

    Added API credentials

--- bubble-burst-sell.py
+++ bubble-burst-sell.py
@@ -7,4 +7,14 @@
 # Sells crypto when the bubble bursts
 # 
 
-import coinbase
+
+# Imports
+import os, coinbase
+from dotenv import load_dotenv
+
+
+# Load API credentials from .env
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+
